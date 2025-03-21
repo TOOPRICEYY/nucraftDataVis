3d313c4b001cf2f51048fe94c354d02bacd79b0c
Gabriel Price <starfinderjetpacks@gmail.com>
2025-03-21 14:17:42 -0400

    done

---
+++ error-dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+import ErrorDataVisualization from './components/ErrorDataVisualization'
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorDataVisualization />
+    </div>
+  )
+}
+
+export default App
